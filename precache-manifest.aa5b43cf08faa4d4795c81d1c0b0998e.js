8972f975c75edc0bd3391ae7242598dac58f2f56
webDevSaurav <webdevelopersaurav@gmail.com>
2020-08-08 17:31:58 +0530

    Updates

---
+++ precache-manifest.aa5b43cf08faa4d4795c81d1c0b0998e.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba9d5eb3d6ec6cf69d0c7793100914c5",
+    "url": "/food-hacathon/index.html"
+  },
+  {
+    "revision": "5e0575ed7136b0e088ff",
+    "url": "/food-hacathon/static/css/main.09bc1fe7.chunk.css"
+  },
+  {
+    "revision": "446e929e5fa636830238",
+    "url": "/food-hacathon/static/js/2.386b899f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/food-hacathon/static/js/2.386b899f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5e0575ed7136b0e088ff",
+    "url": "/food-hacathon/static/js/main.04bfb4fe.chunk.js"
+  },
+  {
+    "revision": "7fec7c64a4cd604f9e21",
+    "url": "/food-hacathon/static/js/runtime-main.c4bc296c.js"
+  },
+  {
+    "revision": "7cd92069f47b48e50dedd69f7a704558",
+    "url": "/food-hacathon/static/media/Illustration1.7cd92069.png"
+  },
+  {
+    "revision": "dfb2ea3c2ab3a8dafafe2226ac6fb008",
+    "url": "/food-hacathon/static/media/Illustration2.dfb2ea3c.png"
+  },
+  {
+    "revision": "de55ae52af85b8952e65d1b546992618",
+    "url": "/food-hacathon/static/media/Metropolis-Black.de55ae52.otf"
+  }
+]);
